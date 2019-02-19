df612d89cba25f2f5926aee50b9ad43babf9c038
Mihai <kazepworld@gmail.com>
2019-02-19 20:55:32 +0100

    Add basic command implementation

---
+++ FluidIrc/ViewModels/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FluidIrc.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+
+        private readonly Action _action;
+        private readonly bool _canExecute;
+
+        public RelayCommand(Action action, bool canExecute = true)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action?.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
