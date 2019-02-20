d862f6666613053f6e4a2d8d02048297976188be
Mihai <kazepworld@gmail.com>
2019-02-20 01:24:16 +0100

    Add control for displaying connected users to a channel

---
+++ FluidIrc/Controls/UsersPanel.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace FluidIrc.Controls
+{
+    public sealed partial class UsersPanel : UserControl
+    {
+        public UsersPanel()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ FluidIrc/ViewModels/UsersPanelViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FluidIrc.ViewModels
+{
+    public class UsersPanelViewModel : ViewModelBase
+    {
+
+        public ObservableCollection<string> ConnectedUsers { get; set; }
+
+    }
+}
