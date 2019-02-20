4a206f2abecaec2b9968dbf006c3fb739b36383f
Mihai <kazepworld@gmail.com>
2019-02-20 01:23:20 +0100

    Add control handling user text inputs

---
+++ FluidIrc/Controls/MessageBar.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace FluidIrc.Controls
+{
+    public sealed partial class MessageBar : UserControl
+    {
+        public MessageBar()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ FluidIrc/ViewModels/MessageBarViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace FluidIrc.ViewModels
+{
+    public class MessageBarViewModel : ViewModelBase
+    {
+
+        private string _commandText;
+        public string CommandText
+        {
+            get => _commandText;
+            set => Set(ref _commandText, value);
+        }
+
+        public ICommand SendCommand { get; set; }
+
+        public MessageBarViewModel()
+        {
+
+        }
+
+    }
+}
