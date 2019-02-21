ece931dd7eb15f584b2ef0dbfaf33456455a9320
Mihai <kazepworld@gmail.com>
2019-02-21 20:26:34 +0100

    Migrate homepage code-behind to Prism

--- FluidIrc/Views/HomePage.xaml.cs
+++ FluidIrc/Views/HomePage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using FluidIrc.ViewModels;
-using Windows.UI.Xaml.Controls;
+using Prism.Windows.Mvvm;
+using System.ComponentModel;
+using Windows.UI.Xaml;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -8,12 +10,21 @@ namespace FluidIrc.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HomePage : Page
+    public sealed partial class HomePage : SessionStateAwarePage, INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public HomePageViewModel ConcreteDataContext => DataContext as HomePageViewModel;
+
         public HomePage()
         {
-            this.InitializeComponent();
-            DataContext = new HomePageViewModel();
+            InitializeComponent();
+            DataContextChanged += OnDataContextChanged;
+        }
+
+        private void OnDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ConcreteDataContext)));
         }
     }
 }
