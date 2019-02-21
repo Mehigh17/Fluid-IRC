7e1e90210e791f1c2daf4e1e12cabac9e9ac8f69
Mihai <kazepworld@gmail.com>
2019-02-21 13:49:47 +0100

    Add view for the home page

---
+++ FluidIrc/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FluidIrc.ViewModels
+{
+    public class HomePageViewModel : ViewModelBase
+    {
+
+        public ObservableCollection<ServerCardViewModel> ServerCards { get; set; } = new ObservableCollection<ServerCardViewModel>();
+
+        public HomePageViewModel()
+        {
+            
+        }
+
+    }
+}
---
+++ FluidIrc/Views/HomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FluidIrc.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FluidIrc.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+            DataContext = new HomePageViewModel();
+        }
+    }
+}
