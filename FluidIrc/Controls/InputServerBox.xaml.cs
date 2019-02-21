285eda6a2ec884250970aecf3bb599a99a49f786
Mihai <kazepworld@gmail.com>
2019-02-21 22:30:29 +0100

    Implement server creation page with needed controls

---
+++ FluidIrc/Controls/InputServerBox.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace FluidIrc.Controls
+{
+    public sealed partial class InputServerBox : UserControl
+    {
+        public InputServerBox()
+        {
+            this.InitializeComponent();
+        }
+
+        /*
+         * Prevent user to input any character besides numeric ones.
+         */
+        private void PortTextBox_OnBeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            if(!string.IsNullOrEmpty(args.NewText))
+                args.Cancel = !int.TryParse(args.NewText.Last().ToString(), out _);
+        }
+    }
+}
