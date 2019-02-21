e887259db71a162de435eb2703c268b9cdea2327
Mihai <kazepworld@gmail.com>
2019-02-21 13:49:15 +0100

    Add control for server cards

---
+++ FluidIrc/ViewModels/ServerCardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FluidIrc.ViewModels
+{
+    public class ServerCardViewModel : ViewModelBase
+    {
+
+        public string ServerName { get; set; }
+        public string ServerAddress { get; set; }
+
+    }
+}
