daf0239b5133f2577eb8b060db2b42f633508a7d
Mihai <kazepworld@gmail.com>
2019-02-21 21:10:56 +0100

    Add the app shell containing the base layout

--- FluidIrc/App.xaml.cs
+++ FluidIrc/App.xaml.cs
@@ -1,91 +1,39 @@
-﻿using System;
-using Windows.ApplicationModel;
+﻿using Prism.Unity.Windows;
+using System.Threading.Tasks;
+using Unity;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
-using FluidIrc.Views;
 
 namespace FluidIrc
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    public sealed partial class App : PrismUnityApplication
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            InitializeComponent();
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected override UIElement CreateShell(Frame rootFrame)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
-            }
+            var shell = Container.Resolve<AppShell>();
+            shell.SetContentFrame(rootFrame);
+            return shell;
         }
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
         {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+            //Container.RegisterInstance<IResourceLoader>(new ResourceLoaderAdapter(new ResourceLoader()));
+            return base.OnInitializeAsync(args);
         }
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
         {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
+            NavigationService.Navigate(PageTokens.Home.ToString(), null);
+            return Task.FromResult(true);
         }
     }
 }
---
+++ FluidIrc/AppShell.xaml.cs
@@ -0,0 +1,53 @@
+﻿using FluidIrc.ViewModels;
+using Prism.Windows.Mvvm;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FluidIrc
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppShell : SessionStateAwarePage, INotifyPropertyChanged
+    {
+
+        public AppShell(AppShellViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContextChanged += OnDataContextChanged;
+
+            DataContext = viewModel;
+        }
+
+        public void SetContentFrame(Frame frame)
+        {
+            ContentFrame.Content = frame;
+        }
+
+        public AppShellViewModel ConcreteDataContext => DataContext as AppShellViewModel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ConcreteDataContext)));
+        }
+
+        private void NavigationView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+
+            }
+            else
+            {
+                var item = (NavigationViewItem) args.InvokedItemContainer;
+                var itemVm = item.DataContext as NavigationItemViewModel;
+                itemVm?.Command.Execute(null);
+            }
+        }
+    }
+}
