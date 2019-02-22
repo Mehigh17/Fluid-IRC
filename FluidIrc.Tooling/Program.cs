b0d48726cbe5c69813fb8f93944c33f430f16649
Mihai <kazepworld@gmail.com>
2019-02-22 01:28:05 +0100

    Create a dummy project to support EF Core tooling

---
+++ FluidIrc.Tooling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FluidIrc.Tooling
+{
+    class Program
+    {
+
+        /************************************************************************************************************
+         * The sole purpose of this project is to make the EF Core Tooling library work.                            *
+         * It doesn't fully support UWP yet so we need a dummy project to create the migrations.                    *
+         *                                                                                                          *
+         * !!! Other than that this project servers no purpose and shouldn't be deployed with the final build !!!    *
+         ************************************************************************************************************/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
