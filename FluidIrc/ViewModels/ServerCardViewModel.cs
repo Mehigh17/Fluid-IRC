198d6ac8e6bd00f67f268cf794eb9446f5bfecb6
Mihai <kazepworld@gmail.com>
2019-02-26 00:38:46 +0100

    Raise property notification events in the server card viewmodel

--- FluidIrc/ViewModels/ServerCardViewModel.cs
+++ FluidIrc/ViewModels/ServerCardViewModel.cs
@@ -1,10 +1,39 @@
-﻿namespace FluidIrc.ViewModels
+﻿using Prism.Windows.Mvvm;
+using System.Windows.Input;
+
+namespace FluidIrc.ViewModels
 {
     public class ServerCardViewModel : ViewModelBase
     {
 
-        public string ServerName { get; set; }
-        public string ServerAddress { get; set; }
+        private string _serverName;
+        public string ServerName
+        {
+            get => _serverName;
+            set => SetProperty(ref _serverName, value);
+        }
+
+
+        private string _serverAddress;
+        public string ServerAddress
+        {
+            get => _serverAddress;
+            set => SetProperty(ref _serverAddress, value);
+        }
+
+        private ICommand _command;
+        public ICommand Command
+        {
+            get => _command;
+            set => SetProperty(ref _command, value);
+        }
+
+        private ICommand _deleteCommand;
+        public ICommand DeleteCommand
+        {
+            get => _deleteCommand;
+            set => SetProperty(ref _deleteCommand, value);
+        }
 
     }
 }
