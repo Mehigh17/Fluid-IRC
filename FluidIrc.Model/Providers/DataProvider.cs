5915bfed65bad0df29c2e59d2356e4a778f93d70
Mihai <kazepworld@gmail.com>
2019-02-25 03:06:26 +0100

    Add base data providers

---
+++ FluidIrc.Model/Providers/DataProvider.cs
@@ -0,0 +1,74 @@
+﻿using FluidIrc.Model.Data;
+using FluidIrc.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FluidIrc.Model.Providers
+{
+    public class DataProvider : IDataProvider
+    {
+
+        private readonly IApplicationContextFactory _appContextFactory;
+
+        public DataProvider(IApplicationContextFactory appContextFactory)
+        {
+            _appContextFactory = appContextFactory;
+        }
+
+        public async Task AddServer(Server server)
+        {
+            using (var context = _appContextFactory.CreateApplicationContext())
+            {
+                if (await context.Servers.FindAsync(server.Id) == null)
+                {
+                    await context.Servers.AddAsync(server);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
+        public async Task RemoveServer(Guid serverId)
+        {
+            using (var context = _appContextFactory.CreateApplicationContext())
+            {
+                var server = await context.Servers.FindAsync(serverId);
+                if (server != null)
+                {
+                    context.Servers.Remove(server);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
+        public Task RemoveServer(Server server)
+        {
+            return RemoveServer(server.Id);
+        }
+
+        public async Task<List<Server>> GetServers()
+        {
+            using (var context = _appContextFactory.CreateApplicationContext())
+            {
+                return await context.Servers.Include(s => s.UserProfile).ToListAsync();
+            }
+        }
+
+        public async Task<Server> GetServer(Guid id)
+        {
+            using (var context = _appContextFactory.CreateApplicationContext())
+            {
+                return await context.Servers.Include(s => s.UserProfile).FirstOrDefaultAsync(s => s.Id.Equals(id));
+            }
+        }
+
+        public async Task<List<UserProfile>> GetProfiles()
+        {
+            using (var context = _appContextFactory.CreateApplicationContext())
+            {
+                return await context.UserProfiles.ToListAsync();
+            }
+        }
+    }
+}
---
+++ FluidIrc.Model/Providers/IDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluidIrc.Model.Models;
+
+namespace FluidIrc.Model.Providers
+{
+    public interface IDataProvider
+    {
+
+        Task AddServer(Server server);
+
+        Task RemoveServer(Guid serverId);
+        Task RemoveServer(Server server);
+
+        Task<List<Server>> GetServers();
+        Task<Server> GetServer(Guid id);
+
+        Task<List<UserProfile>> GetProfiles();
+
+    }
+}
