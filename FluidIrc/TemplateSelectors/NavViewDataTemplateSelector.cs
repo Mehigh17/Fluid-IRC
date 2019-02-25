f9f7cac3d0d26aba67d84ffe24ff04bf6fdc9951
Mihai <kazepworld@gmail.com>
2019-02-26 00:22:13 +0100

    Add the navigation data template selector

---
+++ FluidIrc/TemplateSelectors/NavViewDataTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using FluidIrc.ViewModels.Navigation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace FluidIrc.TemplateSelectors
+{
+    public class NavViewDataTemplateSelector : DataTemplateSelector
+    {
+
+        public DataTemplate GotoItemDataTemplate { get; set; }
+        public DataTemplate ChannelDataTemplate { get; set; }
+        public DataTemplate HeaderDataTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if (item is NavigationGotoItemViewModel)
+                return GotoItemDataTemplate;
+            if (item is NavigationHeaderViewModel)
+                return HeaderDataTemplate;
+            if (item is NavigationChannelViewModel)
+                return ChannelDataTemplate;
+
+            return base.SelectTemplateCore(item);
+        }
+    }
+}
